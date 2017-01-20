b8180b3926cb7ba473bbb70e418cf6bc17a28a0c
hewen <hewen@umich.edu>
2017-01-20 00:15:21 -0500

    pruning algorithm added

--- app/src/main/java/com/example/hewen/restauranthistory/MainActivity.java
+++ app/src/main/java/com/example/hewen/restauranthistory/MainActivity.java
@@ -5,7 +5,9 @@ import java.io.InputStream;
 import java.util.*;
 import java.text.SimpleDateFormat;
 
+import android.graphics.Color;
 import android.os.AsyncTask;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.provider.CallLog;
 import android.support.v7.app.AppCompatActivity;
@@ -27,10 +29,21 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+
 public class MainActivity extends AppCompatActivity {
-    private ListView mLvShow;
-    private List<Map<String, String>> dataList;
-    private SimpleAdapter adapter;
+    //private ListView mLvShow;
+    //private List<Map<String, String>> dataList;
+    //private SimpleAdapter adapter;
     private static final String TAG = "ASYNC_TASK";
 
     private Button execute;
@@ -38,15 +51,73 @@ public class MainActivity extends AppCompatActivity {
     private MyTask mTask;
     private ProgressBar progressBar;
     private TextView textView;
-    private List<String> phoneNumberList;
+    private List<Map<String,String>> phoneNumberList;
     private Button place;
+    private Button histogram;
 
     private String comInfo;
+
+    private BarChart barChart;
+    private XAxis xAxis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        histogram = (Button) findViewById(R.id.histogram);
+        histogram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                barChart = (BarChart)findViewById(R.id.barChart);
+                xAxis = barChart.getXAxis();
+                xAxis.setDrawAxisLine(true);
+                xAxis.setDrawGridLines(false);
+                // x-axis
+                barChart.setDrawGridBackground(true); // whether or not to display the color of background
+                barChart.getAxisLeft().setDrawAxisLine(false);
+                barChart.setTouchEnabled(true);
+                barChart.setDragEnabled(true);
+                barChart.setScaleEnabled(true);
+                // y-axis
+                barChart.setDescription("Number of Calls");
+                barChart.getAxisLeft().setEnabled(false);
+                barChart.getAxisRight().setEnabled(false);
+                Legend legend = barChart.getLegend();// hide legend
+                legend.setEnabled(false);
+
+                ArrayList<String> xValues = new ArrayList<String>();
+                xValues.add("一季度");
+                xValues.add("二季度");
+                xValues.add("三季度");
+                xValues.add("四季度");
+
+                xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);//数据位于底部
+                ArrayList<BarEntry> yValues = new ArrayList<BarEntry>();
+
+                yValues.add(new BarEntry(20, 0));
+                yValues.add(new BarEntry(18, 1));
+                yValues.add(new BarEntry(4, 2));
+                yValues.add(new BarEntry(45, 3));
+
+                BarDataSet barDataSet=new BarDataSet(yValues,"");
+                barDataSet.setValueFormatter(new ValueFormatter() {
+                    @Override
+                    public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+                        int n = (int) value;
+                        return n + " ";
+                    }
+                });
+                //6、设置柱状图的颜色
+                barDataSet.setColors(new int[]{Color.rgb(247, 211, 149), Color.rgb(209, 252, 153),
+                        Color.rgb(161, 232, 251), Color.rgb(239, 147, 156)});
+                //7、显示，柱状图的宽度和动画效果
+                BarData barData = new BarData(xValues, barDataSet);
+                barDataSet.setBarSpacePercent(40f);//值越大，柱状图就越宽度越小；
+                barChart.animateY(1000);
+                barChart.setData(barData); //
+            }
+        });
 
         final String google_url_front = "https://maps.googleapis.com/maps/api/place/search/json?";
         final String google_url_back = "&key=AIzaSyDt3UWjvleAATeUilvewMm5gYpTDCsNgeY";
@@ -60,7 +131,11 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.text_view);
         place.setEnabled(false);
 
-        phoneNumberList = getPhoneNumberList();
+        phoneNumberList = getDataList();
+
+        for (int i = 0 ; i < phoneNumberList.size() ; i++){
+            textView.setText(phoneNumberList.get(i).get("number"));
+        }
 
         execute.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -208,64 +283,62 @@ public class MainActivity extends AppCompatActivity {
                 , null, null, CallLog.Calls.DEFAULT_SORT_ORDER // show from the most recently call to the least recently call
         );
         List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+        int total_duration = 0;
+        int total_calls = 0;
+        Map<String, Integer> call_times = new HashMap<>();
         while (cursor.moveToNext()) {
+            total_calls++;
             String name = cursor.getString(cursor.getColumnIndex(CallLog.Calls.CACHED_NAME));
-            String number = cursor.getString(cursor.getColumnIndex(CallLog.Calls.NUMBER));
-            long dateLong = cursor.getLong(cursor.getColumnIndex(CallLog.Calls.DATE));
-            String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(dateLong));
-            int duration = cursor.getInt(cursor.getColumnIndex(CallLog.Calls.DURATION));
-            int type = cursor.getInt(cursor.getColumnIndex(CallLog.Calls.TYPE));
-            String typeString = "";
-            switch (type) {
-                case CallLog.Calls.INCOMING_TYPE:
-                    typeString = "Incoming";
-                    break;
-                case CallLog.Calls.OUTGOING_TYPE:
-                    typeString = "Outgoing";
-                    break;
-                case CallLog.Calls.MISSED_TYPE:
-                    typeString = "Missed";
-                    break;
-                case CallLog.Calls.REJECTED_TYPE:
-                    typeString = "Rejected";
-                    break;
-                default:
-                    break;
+            if (name == null){
+                // if the number is saved in the phone, then we prune that out (unlikely to be the business)
+                continue;
             }
-            Map<String, String> map = new HashMap<String, String>();
-            map.put("name", (name == null) ? "Unknown" : name);
+            String number = cursor.getString(cursor.getColumnIndex(CallLog.Calls.NUMBER));
+            if (call_times.containsKey(number)){
+                int num = call_times.get(number);
+                call_times.put(number, num++);
+            }
+            else{
+                call_times.put(number, 1);
+            }
+            long dateLong = cursor.getLong(cursor.getColumnIndex(CallLog.Calls.DATE));
+            //String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(dateLong));
+            int duration = cursor.getInt(cursor.getColumnIndex(CallLog.Calls.DURATION));
+            total_duration += duration; // in order to calculate the average duration
+            Map<String, String> map = new HashMap<>();
             map.put("number", number);
-            map.put("date", date);
-            map.put("duration", (duration / 60) + "min");
-            map.put("type", typeString);
+            map.put("duration", duration+"");
+            //map.put("date", date);
             list.add(map);
         }
-        return list;
-    }
+        int avg_duration = total_duration / total_calls;
+        List<String> prune_out_list = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry: call_times.entrySet()){
+            if (entry.getValue() > 1 / 3 * total_calls){
+                // if call more than 1/3 of the total times, prune that out
+                prune_out_list.add(entry.getKey());
+            }
+        }
 
-    private List<String> getPhoneNumberList(){
-        ContentResolver resolver = getContentResolver();
-        Cursor cursor = resolver.query(CallLog.Calls.CONTENT_URI, // URI for searching phone call history
-                new String[] { CallLog.Calls.CACHED_NAME // contact name of the number you dialed
-                        , CallLog.Calls.NUMBER // phone number of the phone call history
-                        , CallLog.Calls.DATE // date of the phone call
-                        , CallLog.Calls.DURATION // duration of the phone call
-                        , CallLog.Calls.TYPE } // type of the pnoe call
-                , null, null, CallLog.Calls.DEFAULT_SORT_ORDER // show from the most recently call to the least recently call
-        );
-        List<String> list = new ArrayList<>();
-        while(cursor.moveToNext()){
-            String number = cursor.getString(cursor.getColumnIndex(CallLog.Calls.NUMBER));
-            list.add(number);
+        for (int i = 0 ; i < list.size() ; i++){
+            if (Integer.parseInt(list.get(i).get("duration")) > avg_duration){
+                //if the duration is long, prune that out
+                list.remove(i);
+                continue;
+            }
+            String number = list.get(i).get("number");
+            if (prune_out_list.contains(number)){
+                list.remove(i);
+            }
         }
         return list;
     }
 
-    public void export_click(View view){
+    /*public void export_click(View view){
         mLvShow = (ListView) findViewById(R.id.lv_show);
         dataList = getDataList();
         adapter = new SimpleAdapter(this, dataList, R.layout.simple_callog_item, new String[]{"name", "number", "date", "duration", "type"}
                 , new int[]{R.id.tv_name, R.id.tv_number, R.id.tv_date, R.id.tv_duration, R.id.tv_type});
         mLvShow.setAdapter(adapter);
-    }
+    }*/
 }
